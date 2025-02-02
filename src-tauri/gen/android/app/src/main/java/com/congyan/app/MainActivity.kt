02976be5bef1a32d5ed17b3ba5201cff10e8b57a
MengChen <tatsukimengchen@163.com>
2025-02-03 01:38:32 +0800

    improvement: 优化安卓 APP 退出逻辑

--- src-tauri/gen/android/app/src/main/java/com/congyan/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/congyan/app/MainActivity.kt
@@ -3,10 +3,18 @@ package com.congyan.app
 import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.KeyEvent
 import android.view.View
 import android.webkit.WebView
+import android.widget.Toast
 
 class MainActivity : TauriActivity() {
+  private var doubleBackToExitPressedOnce = false
+  private val handler = Handler(Looper.getMainLooper())
+  private lateinit var mWebView: WebView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
@@ -26,6 +34,7 @@ class MainActivity : TauriActivity() {
   @SuppressLint("SetJavaScriptEnabled")
   override fun onWebViewCreate(webView: WebView) {
     super.onWebViewCreate(webView)
+    mWebView = webView
     val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
     val color = when (nightModeFlags) {
       Configuration.UI_MODE_NIGHT_YES -> 0xFF1a1c1e.toInt()
@@ -35,6 +44,27 @@ class MainActivity : TauriActivity() {
     webView.setBackgroundColor(color)
     webView.settings.javaScriptEnabled = true
     webView.addJavascriptInterface(WebAppInterface(this), "Android")
+  }
+
+  override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      if (mWebView.canGoBack()) {
+        mWebView.goBack()
+        return true
+      }
+
+      if (doubleBackToExitPressedOnce) {
+        super.onBackPressed()
+        return true
+      }
+
+      this.doubleBackToExitPressedOnce = true
+      Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show()
+
+      handler.postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+      return true
+    }
+    return super.onKeyDown(keyCode, event)
   }
 
   private fun updateStatusBarColor() {
